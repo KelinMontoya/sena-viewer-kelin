419689915bc88241ab44f4161480cc529a6106cb
KelinMontoya <kellymontoya91@gmail.com>
2025-12-12 11:23:57 -0500

    feat: Usar content.toString() o crear un método real de play

--- src/com/senaviewer/player/Player.java
+++ src/com/senaviewer/player/Player.java
@@ -3,15 +3,20 @@ package com.senaviewer.player;
 import com.senaviewer.models.Content;
 
 public class Player {
+
     private Content current;
 
     public void play(Content content) {
-    System.out.println("▶ Reproduciendo: " + content.getTitle());
-        current = content;
+        System.out.println("▶ Reproduciendo: " + content.getTitle());
+        
+        current = content; // ✔ Corrección
+
         System.out.println("--- Player: Iniciando reproducción ---");
-        content.play();
+        System.out.println(content.toString()); // ✔ Muestra la info del contenido
         System.out.println("--- Player: Fin ---\n");
     }
 
-    public Content getCurrent() { return current; }
+    public Content getCurrent() {
+        return current;
+    }
 }
